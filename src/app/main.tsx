b2427a80b68b6e5ef8831c63b065485d5a0fa96f
ZoirJohn <zoirbekzokirjonov2007@gmail.com>
2026-08-20 22:45:34 +0500

    change architecture of the project to implement FSD

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import App from '@/app/App.tsx'
+import "@/app/main.css"
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById('wrapper')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
