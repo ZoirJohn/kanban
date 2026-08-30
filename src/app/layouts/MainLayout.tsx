eb181075fabe8d90b2616ff2f306181990f804be
umidjon <umidasirov55@gmail.com>
2026-08-30 20:28:11 +0500

    added sidebar

--- src/app/layouts/MainLayout.tsx
+++ src/app/layouts/MainLayout.tsx
@@ -1,13 +1,12 @@
 import { Outlet } from "react-router-dom";
 import { Navbar } from "../../widgets/Navbar/ui/Navbar";
-import { Container } from "@mantine/core";
 export function MainLayout() {
 	return (
 		<>
 			<Navbar />
-			<Container size={"xl"}>
+			<main>
 				<Outlet />
-			</Container>
+			</main>
 		</>
 	);
 }
---
+++ src/pages/Kanban/Timeline/TimeLine.tsx
@@ -0,0 +1,3 @@
+export function TimeLine() {
+	return <div>TimeLine</div>;
+}
